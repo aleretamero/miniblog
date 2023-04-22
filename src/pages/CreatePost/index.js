a4c596e19930bf83bb2e9f7db30a987202967102
Alexandre Retamero <alexandreretamero@gmail.com>
2023-04-22 19:18:05 -0300

    Create index.js

---
+++ src/pages/CreatePost/index.js
@@ -0,0 +1,121 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuthValue } from '../../context/AuthContext';
+import { useInsertDocument } from '../../hooks/useInsertDocument';
+
+import styles from './styles.module.css';
+
+const CreatePost = () => {
+  const [title, setTitle] = useState('');
+  const [image, setImage] = useState('');
+  const [body, setBody] = useState('');
+  const [tags, setTags] = useState([]);
+  const [formError, setFormError] = useState('');
+
+  const { user } = useAuthValue();
+  const { insertDocument, response } = useInsertDocument('posts');
+
+  const navigate = useNavigate();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setFormError('');
+
+    // validate image URL
+    try {
+      new URL(image);
+    } catch (error) {
+      setFormError('A Imagem precisa ser uma URL.');
+    }
+
+    // criar o array de tags
+    const tagsArray = tags.split(',').map(tags => tags.trim().toLowerCase());
+
+    // checar todos os valores
+    if (!title || !image || !body || !tags) {
+      setFormError('Por favor, preencha todos os campos');
+    }
+
+    if (formError) return;
+
+    insertDocument({
+      title,
+      image,
+      body,
+      tagsArray,
+      uid: user.uid,
+      createdBy: user.displayName,
+    });
+
+    setTitle('');
+    setImage('');
+    setBody('');
+    setTags('');
+
+    // redirect to home page
+    navigate('/');
+  };
+
+  return (
+    <div className={styles.createPost}>
+      <h2>Criar post</h2>
+      <p>Escreva sobre o que quiser e compartilhe o seu conhecimento!</p>
+      <form onSubmit={handleSubmit} autoComplete="off">
+        <label>
+          <span>Título:</span>
+          <input
+            type="text"
+            name="title"
+            required
+            placeholder="Pense num bom título..."
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>URL da imagem:</span>
+          <input
+            type="text"
+            name="image"
+            required
+            placeholder="Insira uma imagem que representa o seu post"
+            value={image}
+            onChange={e => setImage(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>Conteúdo:</span>
+          <textarea
+            name="body"
+            required
+            placeholder="Insira o conteúdo do post"
+            value={body}
+            onChange={e => setBody(e.target.value)}
+          ></textarea>
+        </label>
+        <label>
+          <span>Tags:</span>
+          <input
+            type="text"
+            name="tags"
+            required
+            placeholder="Insira as tags separadas por vírgula"
+            value={tags}
+            onChange={e => setTags(e.target.value)}
+          />
+        </label>
+
+        {!response.loading && <button className="btn">Cadastrar</button>}
+        {response.loading && (
+          <button className="btn" disabled>
+            Aguarde...
+          </button>
+        )}
+        {response.error && <p className="error">{response.error}</p>}
+        {formError && <p className="error">{formError}</p>}
+      </form>
+    </div>
+  );
+};
+
+export default CreatePost;
