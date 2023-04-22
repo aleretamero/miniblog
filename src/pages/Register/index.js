9643bce76a3385cb8447777b6c59f7fcac44e6c1
Alexandre Retamero <alexandreretamero@gmail.com>
2023-04-22 14:45:46 -0300

    Create index.js

---
+++ src/pages/Register/index.js
@@ -0,0 +1,57 @@
+import styles from './styles.module.css';
+
+import { useState, useEffect } from 'react';
+
+const Register = () => {
+  return (
+    <div className={styles.register}>
+      <h1>Cadastre-se para postar</h1>
+      <p>Crie seu usuário e compartilhe suas histórias</p>
+      <form>
+        <label>
+          <span>Nome:</span>
+          <input
+            type="text"
+            name="displayName"
+            required
+            placeholder="Nome do usuário"
+            autoComplete='name'
+          />
+        </label>
+        <label>
+          <span>E-mail:</span>
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="E-mail do usuário"
+            autocomplete="email"
+          />
+        </label>
+        <label>
+          <span>Senha:</span>
+          <input
+            type="password"
+            name="password"
+            required
+            placeholder="Insira sua senha"
+            autocomplete="new-password"
+          />
+        </label>
+        <label>
+          <span>Confirmação de senha:</span>
+          <input
+            type="password"
+            name="confirmPassword"
+            required
+            placeholder="Confirme a sua senha"
+            autocomplete="new-password"
+          />
+        </label>
+        <button className="btn">Cadastrar</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
