670249e55f3d34b247d2eeeda1497f66b34e808d
Alexandre Retamero <alexandreretamero@gmail.com>
2023-04-23 00:52:38 -0300

    Update useFetchDocuments.js

--- src/hooks/useFetchDocuments.js
+++ src/hooks/useFetchDocuments.js
@@ -32,7 +32,21 @@ export const useFetchDocuments = (docCollection, search = null, uid = null) => {
         // busca
         // dashboard
 
-        q = await query(collectionRef, orderBy('createdAt', 'desc'));
+        if (search) {
+          q = await query(
+            collectionRef,
+            where('tagsArray', 'array-contains', search),
+            orderBy('createdAt', 'desc')
+          );
+        } else if(uid) {
+          q = await query(
+            collectionRef,
+            where('uid', "==" , uid),
+            orderBy('createdAt', 'desc')
+          );
+        } else {
+          q = await query(collectionRef, orderBy('createdAt', 'desc'));
+        }
 
         await onSnapshot(q, QuerySnapshot => {
           setDocuments(
