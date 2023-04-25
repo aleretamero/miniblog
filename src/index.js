d3a9e354c0a2a95a88d5949d3074ef133a8f5f69
Alexandre Retamero <alexandreretamero@gmail.com>
2023-04-25 16:29:35 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import './styles/index.css';
+// Bootstrap
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import './styles/index.css';
+import './sass/reset.scss';
 
 import App from './App';
 
