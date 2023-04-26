6edf5b7858161f8675cf5ec96c2cb8a8f4363f70
Alexandre Retamero <alexandreretamero@gmail.com>
2023-04-26 12:56:46 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// Reset CSS
-import './sass/reset.scss';
-
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
