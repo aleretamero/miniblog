d2fbe892ff645486b5031ab7e5d2c0b7154a39d8
Alexandre Retamero <alexandreretamero@gmail.com>
2023-04-27 00:12:11 -0300

    Update index.jsx

--- src/pages/Post/index.jsx
+++ src/pages/Post/index.jsx
@@ -9,17 +9,19 @@ const Post = () => {
     <div className="container min-vh-100 d-flex justify-content-center align-items-center p-1">
       {loading && <p>Carregando post...</p>}
       {post && (
-        <div className="card">
+        <div className="card border border-5 border-dark">
           <img
             src={post.image}
-            className="card-img-top img-fluid"
+            className="img-fluid border-bottom border-2 border-dark"
             alt={post.title}
           />
-          <div className="card-body">
-            <h5 className="card-title">{post.title}</h5>
+          <div className="card-body  d-flex flex-column gap-3">
+            <h5 className="card-title text-center mb-3 fs-1">{post.title}</h5>
             <p className="card-text">{post.body}</p>
-            <p className="card-text">Este post trata sobre:</p>
             <div className="d-flex gap-2">
+              <p className="card-text text-sm fw-bold">
+                Este post trata sobre:
+              </p>
               {post.tagsArray.map(tag => (
                 <p key={tag}>#{tag}</p>
               ))}
